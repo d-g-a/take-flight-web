b24baf10cadbd02569cadb3cce3aa77df76d4870
Diego Guerrero Alexander <65368267+dieglitter@users.noreply.github.com>
2022-02-20 18:01:31 -0600

    anime.js morph test

--- web/src/components/AnimationTest/style.js
+++ web/src/components/AnimationTest/style.js
@@ -5,20 +5,20 @@ export const AnimationTestStyled = styled.div`
    margin: 0;
 
    .polymorph{
-       fill: #29ABE2
+       fill: black;
    }
 
    #cta, #cta2 {
-       position: absolute;
+       //position: absolute;
        margin: 10em auto 0 auto;
        left: 0;
        right: 0;
-       width: 10%;
+       width: 20%;
        background: none;
-       border: 1px solid #fff;
+       border: 1px solid black;
        border-radius: 5px;
        padding: 15px;
-       color: #fff;
+       color: black;
        font-size: 1.5em;
        cursor: pointer;
        outline: 0;
