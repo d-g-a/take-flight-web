dc2f9ecaa96931b335e01b53673059d13384256e
Diego Guerrero Alexander <65368267+dieglitter@users.noreply.github.com>
2022-04-05 23:50:35 -0500

    revert

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,29 +1,11 @@
 import React from "react";
+import { GlobalScroll } from "../components/GlobalScroll";
 import { Layout } from "../components/Layout";
-import { Landing } from "../components/Landing";
-import { SecondaryContent } from "../components/SecondaryContent";
-import { ExperiencePage } from "../components/ExperiencePage";
-import { TeachersPage } from "../components/TeachersPage";
-import { Footer } from "../components/Footer";
 
 const Home = () => {
   return (
     <Layout>
-      <section className="panel landing">
-        <Landing />
-      </section>
-      <section className="panel landing">
-        <SecondaryContent />
-      </section>
-      <section className="panel experience" id="experience_page">
-        <ExperiencePage />
-      </section>
-      <section className="panel teachers">
-        <TeachersPage />
-      </section>
-      <section>
-        <Footer className="panel footer" />
-      </section>
+      <GlobalScroll />
     </Layout>
   );
 };
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,40 +3,17 @@ import SEO from "../components/seo";
 import { AnimationTestTwo } from "../components/AnimationTestTwo";
 import { Layout } from "../components/Layout";
 import { Link } from "gatsby";
-import { Landing } from "../components/Landing";
-import { SecondaryContent } from "../components/SecondaryContent";
-import { ExperiencePage } from "../components/ExperiencePage";
-import { TeachersPage } from "../components/TeachersPage";
-import { Footer } from "../components/Footer";
-import { FloatingFooter } from "../components/FloatingFooter";
 
 const IndexPage = (props) => {
   return (
-    <FloatingFooter>
-      <Layout>
-        <SEO
-        //title={site.title}
-        //description={site.description}
-        //keywords={site.keywords}
-        />
-        <Link to="/home">HOME</Link>
-        <section className="panel landing">
-          <Landing />
-        </section>
-        <section className="panel landing">
-          <SecondaryContent />
-        </section>
-        <section className="panel experience" id="experience_page">
-          <ExperiencePage />
-        </section>
-        <section className="panel teachers">
-          <TeachersPage />
-        </section>
-        <section>
-          <Footer className="panel footer" />
-        </section>
-      </Layout>
-    </FloatingFooter>
+    <Layout>
+      <SEO
+      //title={site.title}
+      //description={site.description}
+      //keywords={site.keywords}
+      />
+      <Link to="/home">HOME</Link>
+    </Layout>
   );
 };
 
