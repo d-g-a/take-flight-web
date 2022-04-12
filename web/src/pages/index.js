7edb7f763fda194e13be1ea4739b9c67b66d75de
Diego Guerrero Alexander <65368267+dieglitter@users.noreply.github.com>
2022-04-12 12:01:01 -0500

    favicon added

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import SEO from "../components/seo";
 import { Layout } from "../components/Layout";
-import { IntroVideo } from "../components/IntroVideo"; 
+import { IntroVideo } from "../components/IntroVideo";
+import { Helmet } from "react-helmet";
+import favicon from "../images/tk_favicon.png";
 
 const IndexPage = (props) => {
   return (
     <Layout>
+      <Helmet>
+        <meta name="icon" href={favicon} />
+      </Helmet>
       <SEO
       //title={site.title}
       //description={site.description}
       //keywords={site.keywords}
       />
-     <IntroVideo />
+      <IntroVideo />
     </Layout>
   );
 };
