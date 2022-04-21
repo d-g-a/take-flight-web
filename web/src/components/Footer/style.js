529992f2f64ecf0387d88120f8fa692abc0222d1
Diego Guerrero Alexander <65368267+dieglitter@users.noreply.github.com>
2022-04-20 19:26:40 -0500

    style fix

--- web/src/components/Footer/style.js
+++ web/src/components/Footer/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import "../../static/fonts/fonts.css";
-import { SM, MD } from "../../constants/mediaQueries";
+import { SM, MD, LG } from "../../constants/mediaQueries";
 
 export const FooterWrapper = styled.div`
   width: 100vw;
@@ -37,6 +37,11 @@ export const FooterWrapper = styled.div`
       a:hover {
         color: #4814cc;
       }
+      @media (max-width: ${LG}) {
+        font-size: 40px;
+        img {
+          width: 100px;
+        }
       @media (max-width: ${MD}) {
         font-size: 40px;
         img {
